eab774ab4e40c342ee2ce63e0a2f8229680a4352
Max Payne <forgge@gmail.com>
2015-08-29 17:40:49 +0300

    Added a placeholder for further C++ concepts use

---
+++ sys/iodev/iodev.hpp
@@ -0,0 +1,4 @@
+// TODO: fill it when C++ concepts will be ready
+// This header will expose IODevLike concept
+// that will specify constraints for every type
+// that acts like input-output device
