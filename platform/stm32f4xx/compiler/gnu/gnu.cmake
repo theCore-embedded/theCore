3e77456acd5114044643c4cc525f3d83bc258033
Max Payne <forgge@gmail.com>
2015-11-06 00:43:06 +0200

    Clean compile flags
    Fat LTO object is required for our toolchain

--- platform/stm32f4xx/compiler/gnu/gnu.cmake
+++ platform/stm32f4xx/compiler/gnu/gnu.cmake
@@ -9,13 +9,6 @@ set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
 set(CC_WARN_FLAGS "-Wall -Wextra -Wpedantic -Werror")
 set(CXX_WARN_FLAGS "${CC_WARN_FLAGS} -Weffc++")
 
-# optimization flags
-set(CC_OPT_FLAGS "-Os -flto=4 ")
-set(CXX_OPT_FLAGS "${CC_OPT_FLAGS}")
-# used in debug mode
-set(CC_NO_OPT_FLAGS "-O0 -g3")
-set(CXX_NO_OPT_FLAGS "${CC_NO_OPT_FLAGS}")
-
 # common flags for current platform
 set(CC_PLATFORM_FLAGS "-ffreestanding -mcpu=cortex-m4 -mthumb -fdata-sections -ffunction-sections -fno-common")
 
@@ -35,11 +28,21 @@ set(CMAKE_C_FLAGS
 set(CMAKE_CXX_FLAGS
 "${CMAKE_CXX_FLAGS} ${CXX_PLATFORM_FLAGS} ${CC_WARN_FLAGS} ${CXX_EXTRA_FLAGS}")
 
-# Default flags of the release mode are OK for our builds
-# Default flags of the minimum size mode are OK for our builds
-# Debug mode
-set(CMAKE_C_FLAGS_DEBUG  "${CC_OPT_FLAGS}")
-set(CMAKE_CXX_FLAGS_DEBUG "${CXX_OPT_FLAGS}")
+# Release flags, optimization is on,
+set(CMAKE_C_FLAGS_RELEASE "-O3 -flto=4 -ffat-lto-objects ")
+set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_C_FLAGS_RELEASE})
+
+# Minimum size release flags, LTO and minimum size
+set(CMAKE_C_FLAGS_MINSIZEREL "-Os -flto=4 -ffat-lto-objects ")
+set(CMAKE_CXX_FLAGS_MINSIZEREL ${CMAKE_C_FLAGS_MINSIZEREL})
+
+# Debug mode, no LTO and maximum debug info
+set(CMAKE_C_FLAGS_DEBUG  "-O0 -g3 ")
+set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG})
+
+# GCC is used as a linker in order to use LTO properly
+set(CMAKE_C_LINKER "arm-none-eabi-gcc")
+set(CMAKE_CXX_LINKER "arm-none-eabi-g++")
 
 # Linker flags
 set(CMAKE_C_LINK_FLAGS
@@ -48,9 +51,9 @@ set(CMAKE_CXX_LINK_FLAGS
 	"-fuse-linker-plugin -nostdlib -nostartfiles -T${CMAKE_CURRENT_LIST_DIR}/stm32.ld -flto -Wl,--gc-sections ")
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 set(CMAKE_C_LINK_EXECUTABLE
-"arm-none-eabi-gcc <OBJECTS> <CMAKE_C_LINK_FLAGS> <LINK_LIBRARIES> -o <TARGET>")
+"${CMAKE_C_LINKER} <OBJECTS> <CMAKE_C_LINK_FLAGS> <LINK_LIBRARIES> -o <TARGET>")
 set(CMAKE_CXX_LINK_EXECUTABLE
-"arm-none-eabi-g++ <OBJECTS> <CMAKE_CXX_LINK_FLAGS> <LINK_LIBRARIES> -o <TARGET>")
+"${CMAKE_CXX_LINKER} <OBJECTS> <CMAKE_CXX_LINK_FLAGS> <LINK_LIBRARIES> -o <TARGET>")
 
 # TODO: merge these flags with compiler one
 set(CMAKE_ASM-ATT_COMPILER_ARG1 "-mcpu=cortex-m4")
