6c40fe7920e46597d4f6fb4b6412b658c5b884e6
Max Payne <forgge@gmail.com>
2016-01-10 23:40:33 +0200

    Add additional core configurations

--- build_api.cmake
+++ build_api.cmake
@@ -6,17 +6,14 @@ set(CORE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 # Registers a project
 macro(register_project project_path platform_name)
+	# TODO: check that platform name is a valid platform name
+	# TODO: check that project path is valid project path
+
 	set(PROJECT_DIR ${project_path})
 	set(PLATFORM_DIR ${CORE_DIR}/platform/${platform_name})
 
-	message("--- > ${project_path}")
-	message("--- > ${PLATFORM_DIR}")
-
-	# Exports platform name
+	# Export the platform name
 	set(PLATFORM_NAME ${platform_name})
-
-	message("--- *> ${PLATFORM_NAME}")
-	message("--- *> ${PROJECT_NAME}")
 
 	# Pick proper compiler definitions
 	include(${PLATFORM_DIR}/compiler/compiler.cmake)
@@ -24,16 +21,8 @@ macro(register_project project_path platform_name)
 	# System headers avaliable for all modules
 	include_directories(${CORE_DIR}/sys/export)
 
-	# Make sure core is included
+	# Make sure the core is included
 	add_subdirectory(${CORE_DIR} ${CMAKE_CURRENT_BINARY_DIR}/core)
-	# Add platform to build
-	add_subdirectory(${PLATFORM_DIR} ${CMAKE_CURRENT_BINARY_DIR}/platform)
-
-	# TODO: find better place for it
-	#add_subdirectory(${CORE_DIR}/sys ${CMAKE_CURRENT_BINARY_DIR}/core/sys)
-	#add_subdirectory(${CORE_DIR}/lib ${CMAKE_CURRENT_BINARY_DIR}/core/lib)
-	#add_subdirectory(${CORE_DIR}/dev ${CMAKE_CURRENT_BINARY_DIR}/core/dev)
-	#add_subdirectory(${CORE_DIR}/kernel ${CMAKE_CURRENT_BINARY_DIR}/core/kernel)
 
 	# Make binary from the project object file
 	add_custom_target(${PROJECT_NAME}.bin ALL
