f32be3ddbdaf1610ecd6fa18bdf5ceee26fc3385
Max Payne <forgge@gmail.com>
2016-02-08 01:43:19 +0200

    Move semaphore test

--- lib/thread/std_host/export/ecl/thread/semaphore.hpp
+++ lib/thread/std_host/export/ecl/thread/semaphore.hpp
@@ -1,6 +1,23 @@
 #ifndef LIB_THREAD_HOST_SEMAPHORE_HPP_
 #define LIB_THREAD_HOST_SEMAPHORE_HPP_
 
-// TODO
+namespace ecl
+{
+
+class semaphore
+{
+public:
+    constexpr semaphore() { }
+
+    void signal() {}
+    void wait() {}
+
+    semaphore(const semaphore&)             = delete;
+    semaphore& operator=(const semaphore&)  = delete;
+
+private:
+};
+
+}
 
 #endif
---
+++ lib/thread/std_host/semaphore.cpp
@@ -0,0 +1,3 @@
+#include <ecl/thread/semaphore.hpp>
+
+
