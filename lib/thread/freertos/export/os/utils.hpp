c14b1c7ce870ca4a5f36a857916d5f88fe3ad69e
Max Payne <forgge@gmail.com>
2016-03-01 23:08:31 +0200

    Add utility for obtaining current thread handle in FreeRTOS
    Many OS-related routines requires current thread id.
    This commit is a preparation for the signal OS primitive.

--- lib/thread/freertos/export/os/utils.hpp
+++ lib/thread/freertos/export/os/utils.hpp
@@ -7,11 +7,32 @@
 namespace os
 {
 
+//! OS-specific handle.
+//! Yet we know that FreeRTOS uses void * as a typedef for TaskHandle_t
+//! this will stay here.
+using thread_handle = void *;
+
 namespace this_thread
 {
 
+//!
+//! \brief Yields current thread.
+//! As for FreeRTOS whether current task will be rescheduled or not
+//! depends on FreeRTOS configuration.
+//!
 void yield();
+
+//!
+//! \brief Put task at sleep for given amount of milliseconds.
+//! \param[in] msesc Milliseconds to sleep.
+//!
 void sleep_for(uint32_t msesc);
+
+//!
+//! \brief Gets a thread handle of currently running thread.
+//! \return Current thread handle
+//!
+thread_handle get_handle();
 
 }
 
--- lib/thread/freertos/utils.cpp
+++ lib/thread/freertos/utils.cpp
@@ -2,6 +2,15 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <platform/utils.hpp>
+
+// Thread Control Block, here is just used as an opaque type
+typedef struct TCB TCB_t;
+
+// Obtain current TCB from FreeRTOS
+extern "C" TCB_t * volatile pxCurrentTCB;
+
+
 
 void os::this_thread::yield()
 {
@@ -11,4 +20,16 @@ void os::this_thread::yield()
 void os::this_thread::sleep_for(uint32_t msecs)
 {
     vTaskDelay(msecs / portTICK_PERIOD_MS);
+}
+
+os::thread_handle os::this_thread::get_handle()
+{
+    TaskHandle_t handle = NULL;
+
+    if (!ecl::in_isr()) {
+        ecl::disable_interrupts();
+        handle = reinterpret_cast< TaskHandle_t >(pxCurrentTCB);
+    }
+
+    return handle;
 }
