7e5263053a861ff672bd4da44f0b54a3c07040b0
Max Payne <forgge@gmail.com>
2016-03-25 16:41:42 +0200

    Rest of dependencies added

--- nix/cpputest/default.nix
+++ nix/cpputest/default.nix
@@ -1,14 +1,34 @@
+# TODO: comments
 { stdenv, fetchFromGitHub }:
 
-with import <nixpkgs> {}; {
-	sdlEnv = stdenv.mkDerivation {
-		name = "cpputest";
-		buildInputs = [ cmake gcc5 autoconf ];
-		src = fetchFromGithHub {
-			ownwer = "cpputest";
-			repo = "cpputest";
-			rev = "0e3b216c7ab365b67765e94aeb45085c4db029e0";
-		};
-	};
+let 
+  pkgs = import <nixpkgs> {};
+in with pkgs; stdenv.mkDerivation {
+  name = "cpputest-3.7.2";
+
+  configurePhase = ''
+  autoreconf -i
+  echo "$prefix"
+  ./configure --prefix=$prefix
+  '';
+
+  buildPhase = ''
+  make
+  '';
+
+  installPhase = ''
+  make install
+  '';
+
+  dontStrip = true;
+
+  buildInputs = [ cmake gcc5 autoconf automake libtool ];
+  src = fetchFromGitHub {
+    owner = "cpputest";
+    repo = "cpputest";
+    rev = "0e3b216c7ab365b67765e94aeb45085c4db029e0";
+    sha256 = "0slljbkzmjhplg36ml1jpilbhvh4kccsbjmmhdimm3hrh2m465zg";
+  };
 }
+
 
