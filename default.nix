7ce178c2ac45c23ccce9bb871556c93b9e67684b
Max Payne <forgge@gmail.com>
2016-04-29 21:17:36 +0300

    Use nix's gcc arm embedded v5.3
    Unused expressions are removed

--- default.nix
+++ default.nix
@@ -2,19 +2,11 @@
 { pkgs ? import <nixpkgs> {}
 }:
 let
-  gcc-arm-embedded = pkgs.callPackage_i686 ./nix/gcc-arm-embedded {
-    dirName = "5.0";
-    subdirName = "5-2015-q4-major";
-    version = "5.2-2015q4-20151219";
-    releaseType = "major";
-    sha256 = "12mbwl9iwbw7h6gwwkvyvfmrsz7vgjz27jh2cz9z006ihzigi50y";
-  };
-
   cpputest = pkgs.callPackage ./nix/cpputest {};
 
 in with pkgs; {
   coreEnv = stdenv.mkDerivation {
     name = "thecore";
-    buildInputs = [ cmake gcc5 cppcheck cpputest gcc-arm-embedded doxygen ];
+    buildInputs = [ cmake gcc5 cppcheck cpputest gcc-arm-embedded-5_2 doxygen ];
   };
 }
