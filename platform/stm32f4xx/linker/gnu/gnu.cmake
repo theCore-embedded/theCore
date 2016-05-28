f2143c8ecd1f5c6e6303c2043a981660af32100b
Max Payne <forgge@gmail.com>
2016-05-28 23:48:27 +0300

    stm32f4xx: standart library included to support long integer arithmetic

--- platform/stm32f4xx/linker/gnu/gnu.cmake
+++ platform/stm32f4xx/linker/gnu/gnu.cmake
@@ -1,9 +1,7 @@
 # Linker flags
 set(CMAKE_C_LINK_FLAGS
-	"-fuse-linker-plugin -nostdlib -nostartfiles -T${CMAKE_CURRENT_LIST_DIR}/stm32.ld \
-	-flto -Wl,--gc-sections "
+	"-fuse-linker-plugin -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mcpu=cortex-m4 \
+	-nostartfiles -mthumb -T${CMAKE_CURRENT_LIST_DIR}/stm32.ld \
+	-flto -Wl,--gc-sections"
 	CACHE STRING "Linker C flags")
-set(CMAKE_CXX_LINK_FLAGS
-	"-fuse-linker-plugin -nostdlib -nostartfiles -T${CMAKE_CURRENT_LIST_DIR}/stm32.ld \
-	-flto -Wl,--gc-sections "
-	CACHE STRING "Linker C++ flags")
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_C_LINK_FLAGS} CACHE STRING "Linker C++ flags")
