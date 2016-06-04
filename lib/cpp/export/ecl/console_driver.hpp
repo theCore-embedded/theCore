9b2f21b8a68f77d2865d8613d9bde0ffd88df87a
Max Payne <forgge@gmail.com>
2016-06-04 23:02:51 +0300

    Move console configuration to TheCore
    Now platform console driver is provided by platform. Application doesn't
    need to maintain additional libraries with console configuration.

---
+++ lib/cpp/export/ecl/console_driver.hpp
@@ -0,0 +1,51 @@
+#ifndef DEFAULT_CONSOLE_DRIVER_HPP_
+#define DEFAULT_CONSOLE_DRIVER_HPP_
+
+#if USE_CONSOLE
+
+#include <platform_console.hpp>
+#include <dev/bus.hpp>
+#include <dev/bus_pipe.hpp>
+
+namespace ecl
+{
+
+// Required aliases
+using console_bus = ecl::generic_bus< platform_console >;
+using console_driver = ecl::bus_pipe< console_bus >;
+
+}
+
+#else // USE_CONSOLE
+
+#include <cstdint>
+#include <unistd.h>
+#include <ecl/err.hpp>
+
+namespace ecl
+{
+
+// Do nothing class. Must be used in case if console driver doesn't needed.
+// Must have the same interface as platform's console driver has
+class console_driver {
+public:
+    ecl::err init() { return ecl::err::ok; }
+
+    ssize_t write(const uint8_t *data, size_t count) {
+        (void) data;
+        (void) count;
+        return 0;
+    }
+
+    ssize_t read(uint8_t *data, size_t count) {
+        (void) data;
+        (void) count;
+        return 0;
+    }
+};
+
+}
+
+#endif // USE_CONSOLE
+
+#endif // DEFAULT_CONSOLE_DRIVER_HPP_
