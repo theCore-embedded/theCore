17e058a5acb0ca585c35ec9bd2c2340b54b75e2d
velichechka <anastasiia.v7@gmail.com>
2016-06-22 21:47:55 +0300

    make changes due to comments in PR-6

--- lib/cpp/tests/istream_unit.cpp
+++ lib/cpp/tests/istream_unit.cpp
@@ -1,208 +1,81 @@
 #include <CppUTest/TestHarness.h>
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTestExt/MockSupport.h>
+#include <sstream>
 
-#include <string>
-#include "ecl/ostream.hpp"
 #include "ecl/istream.hpp"
+#include "mock_device.hpp"
 
-#include <iostream>
-
-class device_t
-{
-public:
-    int test_val = 0;
-    int *p_test_val = &test_val;
-    std::string test_input_str;
-
-    size_t write(uint8_t *c, int size)
-    {
-        *p_test_val += 1;
-        test_input_str.append(reinterpret_cast<char*>(c),
-                              size);
-        return 0;
-    }
-
-    size_t write(const uint8_t *c, int size)
-    {
-        *p_test_val += 1;
-        test_input_str.append(reinterpret_cast<const char*>(c),
-                              size);
-        return 0;
-    }
-
-    size_t read(uint8_t *c, int size)
-    {
-        (void)size;
-        *c = test_input_str[test_val];
-        *p_test_val += 1;
-
-        return 1;
-    }
-};
-
-using ostream_t = ecl::ostream< device_t >;
-using istream_t = ecl::istream< device_t >;
-
-TEST_GROUP(ostream)
-{
-    device_t test_device;
-    ostream_t *test_ostream;
-
-    void setup()
-    {
-        test_ostream = new ostream_t(&test_device);
-    }
-
-    void teardown()
-    {
-        delete test_ostream;
-    }
-};
-
-TEST(ostream, put)
-{
-    test_ostream->put('A');
-    CHECK_EQUAL(1, test_device.test_val);
-    CHECK_EQUAL("A", test_device.test_input_str);
-}
-
-TEST(ostream, write_char)
-{
-    test_ostream->operator<<('B');
-    CHECK_EQUAL(1, test_device.test_val);
-    CHECK_EQUAL("B", test_device.test_input_str);
-}
-
-TEST(ostream, write_string)
-{
-    test_ostream->operator<<("Hello");
-    CHECK_EQUAL(5, test_device.test_val);
-    CHECK_EQUAL("Hello", test_device.test_input_str);
-
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<("Hola, amigos!\n");
-    CHECK_EQUAL(15, test_device.test_val);
-    CHECK_EQUAL("Hola, amigos!\r\n", test_device.test_input_str);
-
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<("");
-    CHECK_EQUAL(0, test_device.test_val);
-    CHECK_EQUAL("", test_device.test_input_str);
-
-    char input_string[] = "123";
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<(input_string);
-    CHECK_EQUAL(3, test_device.test_val);
-    CHECK_EQUAL("123", test_device.test_input_str);
-}
-
-TEST(ostream, write_int)
-{
-    test_ostream->operator<<(12345);
-    CHECK_EQUAL(5, test_device.test_val);
-    CHECK_EQUAL("12345", test_device.test_input_str);
-
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    int input_num = -2147483647;
-    test_ostream->operator<<(input_num);
-    CHECK_EQUAL(11, test_device.test_val);
-    CHECK_EQUAL("-2147483647", test_device.test_input_str);
-
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<(0);
-    CHECK_EQUAL(1, test_device.test_val);
-    CHECK_EQUAL("0", test_device.test_input_str);
-}
-
-TEST(ostream, write_unsigned_int)
-{
-    unsigned int input_num = 4294967295;
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<(input_num);
-    CHECK_EQUAL(10, test_device.test_val);
-    CHECK_EQUAL("4294967295", test_device.test_input_str);
-
-    unsigned int input_num1 = 0;
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<(input_num1);
-    CHECK_EQUAL(1, test_device.test_val);
-    CHECK_EQUAL("0", test_device.test_input_str);
-
-    unsigned int input_num2 = 11111;
-    test_device.test_val = 0;
-    test_device.test_input_str = "";
-    test_ostream->operator<<(input_num2);
-    CHECK_EQUAL(5, test_device.test_val);
-    CHECK_EQUAL("11111", test_device.test_input_str);
-
-}
+using istream_unit = ecl::istream< mock_device >;
 
 TEST_GROUP(istream)
 {
-    device_t test_device;
-    istream_t *test_istream;
+    mock_device test_device;
+    istream_unit *test_istream;
 
     void setup()
     {
-        test_istream = new istream_t(&test_device);
+        test_istream = new istream_unit(&test_device);
+        test_device.idx = 0;
     }
 
     void teardown()
     {
+        mock().clear();
         delete test_istream;
     }
 };
 
-TEST(istream, put)
+TEST(istream, get)
 {
     char input_char;
-    test_device.test_input_str[0] = 'A';
+    test_device.test_input_str = "A";
     test_istream->get(input_char);
-    CHECK_EQUAL(1, test_device.test_val);
     CHECK_EQUAL('A', input_char);
 }
 
 TEST(istream, read_char)
 {
     char output_char;
-    test_device.test_input_str[0] = 'B';
+
+    test_device.test_input_str = "B";
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(1, test_device.test_val);
     CHECK_EQUAL('B', output_char);
 
-    test_device.test_val = 0;
+    test_device.idx = 0;
     test_device.test_input_str = "A B\nC\tD    E\n\n\n\nF";
+
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(1, test_device.test_val);
     CHECK_EQUAL('A', output_char);
 
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(3, test_device.test_val);
     CHECK_EQUAL('B', output_char);
 
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(5, test_device.test_val);
     CHECK_EQUAL('C', output_char);
 
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(7, test_device.test_val);
     CHECK_EQUAL('D', output_char);
 
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(12, test_device.test_val);
     CHECK_EQUAL('E', output_char);
 
     test_istream->operator>>(output_char);
-    CHECK_EQUAL(17, test_device.test_val);
     CHECK_EQUAL('F', output_char);
+}
+
+TEST(istream, read_char_error_case)
+{
+    char output_char = 'A';
+    test_device.test_input_str = "Z";
+    test_device.error_mode = true;
+
+    mock().expectOneCall("read").andReturnValue(-1);
+    test_istream->operator>>(output_char);
+    mock().checkExpectations();
+    CHECK_EQUAL('A', output_char);
+
+    test_device.error_mode = false;
 }
 
 TEST(istream, read_string)
@@ -211,87 +84,197 @@ TEST(istream, read_string)
     char output_string[size] = "\0";
     test_device.test_input_str = "Hello\n";
     test_istream->operator>>(output_string);
-    CHECK_EQUAL(6, test_device.test_val);
     STRCMP_EQUAL("Hello", output_string);
+}
 
-    test_device.test_val = 0;
+TEST(istream, read_string_with_special_chars)
+{
+    const int size = 100;
+    char output_string[size] = "\0";
+
     test_device.test_input_str = " \nHE  L\n";
     test_istream->operator>>(output_string);
-    CHECK_EQUAL(5, test_device.test_val);
     STRCMP_EQUAL("HE", output_string);
+    test_istream->operator>>(output_string);
+    STRCMP_EQUAL("L", output_string);
 
-    test_device.test_val = 0;
+    test_device.idx = 0;
     test_device.test_input_str = "\t   !\\HEL\t\n";
     test_istream->operator>>(output_string);
-    CHECK_EQUAL(10, test_device.test_val);
     STRCMP_EQUAL("!\\HEL", output_string);
+}
 
-    test_device.test_val = 0;
+TEST(istream, read_string_linefeed)
+{
+    const int size = 100;
+    char output_string[size] = "\0";
+
     test_device.test_input_str = "\n";
     test_istream->operator>>(output_string);
-    //CHECK_EQUAL(1, test_device.test_val);
     STRCMP_EQUAL("", output_string);
+}
+
+TEST(istream, read_string_error_case)
+{
+    const int size = 100;
+    char output_string[size] = "SUCCESS";
+    test_device.test_input_str = "ERROR";
+    test_device.error_mode = true;
+
+    mock().expectOneCall("read").andReturnValue(-1);
+    test_istream->operator>>(output_string);
+    mock().checkExpectations();
+    STRCMP_EQUAL("\0", output_string);
+
+    test_device.error_mode = false;
 }
 
 TEST(istream, read_int)
 {
     int output_int;
+    int ss_out;
+    std::basic_stringstream<char> ss;
     test_device.test_input_str = "  123456";
-    test_istream->operator>>(output_int);
-    CHECK_EQUAL(9, test_device.test_val);
-    CHECK_EQUAL(123456, output_int);
 
-    test_device.test_val = 0;
+    ss << test_device.test_input_str;
+    ss >> ss_out;
+    test_istream->operator>>(output_int);
+    CHECK_EQUAL(ss_out, output_int);
+}
+
+TEST(istream, read_int_min)
+{
+    int output_int;
+    int ss_out;
+    std::basic_stringstream<char> ss;
     test_device.test_input_str = "  -2147483647 ";
-    test_istream->operator>>(output_int);
-    CHECK_EQUAL(14, test_device.test_val);
-    CHECK_EQUAL(-2147483647, output_int);
 
-    test_device.test_val = 0;
+    ss << test_device.test_input_str;
+    ss >> ss_out;
+    test_istream->operator>>(output_int);
+    CHECK_EQUAL(ss_out, output_int);
+}
+
+TEST(istream, read_int_with_linefeeds)
+{
+    int output_int;
     test_device.test_input_str = "\n12\n34";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(4, test_device.test_val);
     CHECK_EQUAL(12, output_int);
-
-    test_device.test_val = 0;
-    test_device.test_input_str = "987-654-321";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(4, test_device.test_val);
-    CHECK_EQUAL(987, output_int);
+    CHECK_EQUAL(34, output_int);
+}
 
-    test_device.test_val = 0;
+//TODO: operator>>(int& value) should pass '-' to the next number and make it negative
+//Expected: "987-654-321" should return the following numbers: 987, -654, -321
+//Actual: testcase is successful only with spaces before '-'
+TEST(istream, read_int_with_hyphens)
+{
+    int output_int;
+    int ss_out;
+    std::basic_stringstream<char> ss;
+    test_device.test_input_str = "987 -654 -321";
+    ss << test_device.test_input_str;
+
+    for (int i = 0; i < 3; i++) {
+        ss >> ss_out;
+        test_istream->operator>>(output_int);
+        CHECK_EQUAL(ss_out, output_int);
+    }
+}
+/*
+TEST(istream, read_int_with_hyphens1)
+{
+    int output_int;
+    int ss_out;
+    std::basic_stringstream<char> ss;
+    test_device.test_input_str = "987-654-321";
+    ss << test_device.test_input_str;
+
+    for (int i = 0; i < 3; i++) {
+        ss >> ss_out;
+        test_istream->operator>>(output_int);
+        CHECK_EQUAL(ss_out, output_int);
+    }
+}
+*/
+
+TEST(istream, read_int_zero)
+{
+    int output_int;
     test_device.test_input_str = "0";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(2, test_device.test_val);
     CHECK_EQUAL(0, output_int);
-/*
-    test_device.test_val = 0;
+}
+
+//TODO: INT_MAX can be more than 2 ^ 32.
+//This case becomes useless in such systems
+TEST(istream, read_int_bigger_than_max)
+{
+    int output_int;
+    int ss_out;
+    std::basic_stringstream<char> ss;
     test_device.test_input_str = "214748364700";
+
+    ss << test_device.test_input_str;
+    ss >> ss_out;
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(13, test_device.test_val);
-    CHECK_EQUAL(2147483647, output_int);
-*/
+    CHECK_EQUAL(ss_out, output_int);
+}
+
+TEST(istream, read_int_error_case)
+{
+    int output_int = 123;
+    test_device.test_input_str = "000";
+    test_device.error_mode = true;
+
+    mock().expectOneCall("read").andReturnValue(-1);
+    test_istream->operator>>(output_int);
+    mock().checkExpectations();
+    CHECK_EQUAL(123, output_int);
+
+    test_device.error_mode = false;
 }
 
 TEST(istream, read_unsigned_int)
 {
     unsigned int output_int;
-    test_device.test_input_str = " 4294967295 ";
+    test_device.test_input_str = " 234 ";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(12, test_device.test_val);
-    LONGS_EQUAL(4294967295, output_int);
+    CHECK_EQUAL(234, output_int);
+}
 
-    test_device.test_val = 0;
+TEST(istream, read_unsigned_zero)
+{
+    unsigned int output_int;
     test_device.test_input_str = "\n0\n";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(3, test_device.test_val);
     LONGS_EQUAL(0, output_int);
+}
 
-    test_device.test_val = 0;
+TEST(istream, read_unsigned_consequent)
+{
+    unsigned int output_int;
     test_device.test_input_str = "987\n654";
     test_istream->operator>>(output_int);
-    CHECK_EQUAL(4, test_device.test_val);
     LONGS_EQUAL(987, output_int);
+
+    test_istream->operator>>(output_int);
+    LONGS_EQUAL(654, output_int);
+}
+
+TEST(istream, read_unsigned_int_error_case)
+{
+    unsigned int output_int = 123;
+    test_device.test_input_str = "000";
+    test_device.error_mode = true;
+
+    mock().expectOneCall("read").andReturnValue(-1);
+    test_istream->operator>>(output_int);
+    mock().checkExpectations();
+    CHECK_EQUAL(123, output_int);
+
+    test_device.error_mode = false;
 }
 
 int main(int argc, char *argv[])
--- lib/cpp/tests/ostream_unit.cpp
+++ lib/cpp/tests/ostream_unit.cpp
@@ -1,1 +1,169 @@
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTestExt/MockSupport.h>
+#include <limits.h>
+#include <sstream>
 
+#include "ecl/ostream.hpp"
+#include "mock_device.hpp"
+
+using ostream_unit = ecl::ostream< mock_device >;
+
+TEST_GROUP(ostream)
+{
+    mock_device test_device;
+    ostream_unit *test_ostream;
+
+    void setup()
+    {
+        test_ostream = new ostream_unit(&test_device);
+        test_device.idx = 0;
+        test_device.test_input_str = "";
+        test_device.error_mode = false;
+    }
+
+    void teardown()
+    {
+        mock().clear();
+        delete test_ostream;
+    }
+};
+
+TEST(ostream, put)
+{
+    test_ostream->put('A');
+    CHECK_EQUAL("A", test_device.test_input_str);
+}
+
+TEST(ostream, write_char)
+{
+    test_ostream->operator<<('B');
+    CHECK_EQUAL("B", test_device.test_input_str);
+}
+
+TEST(ostream, write_char_error_case)
+{
+    test_device.error_mode = true;
+
+    mock().expectOneCall("write").andReturnValue(-1);
+    test_ostream->operator<<('A');
+    mock().checkExpectations();
+    CHECK_EQUAL("", test_device.test_input_str);
+}
+
+TEST(ostream, write_string)
+{
+    test_ostream->operator<<("Hello");
+    CHECK_EQUAL("Hello", test_device.test_input_str);
+}
+
+TEST(ostream, write_string_with_linefeed)
+{
+    test_ostream->operator<<("Hola, amigos!\n");
+    CHECK_EQUAL("Hola, amigos!\r\n", test_device.test_input_str);
+}
+
+TEST(ostream, write_empty_string)
+{
+    test_ostream->operator<<("");
+    CHECK_EQUAL("", test_device.test_input_str);
+}
+
+TEST(ostream, write_string_error_case)
+{
+    test_device.error_mode = true;
+
+    mock().expectOneCall("write").andReturnValue(-1);
+    test_ostream->operator<<("ERROR");
+    mock().checkExpectations();
+    CHECK_EQUAL("", test_device.test_input_str);
+}
+
+TEST(ostream, write_int)
+{
+    test_ostream->operator<<(12345);
+    CHECK_EQUAL("12345", test_device.test_input_str);
+}
+
+TEST(ostream, write_int_error_case)
+{
+    test_device.error_mode = true;
+
+    mock().expectOneCall("write").andReturnValue(-1);
+    test_ostream->operator<<(987);
+    mock().checkExpectations();
+    CHECK_EQUAL("", test_device.test_input_str);
+}
+
+TEST(ostream, write_int_min)
+{
+    std::basic_stringstream<char> ss;
+    int input_num = INT_MIN;
+    char out[255];
+
+    ss << input_num;
+    ss.get(out, sizeof(out));
+    test_ostream->operator<<(input_num);
+    CHECK_EQUAL(out, test_device.test_input_str);
+}
+
+TEST(ostream, write_int_max)
+{
+    std::basic_stringstream<char> ss;
+    int input_num = INT_MAX;
+    char out[255];
+
+    ss << input_num;
+    ss.get(out, sizeof(out));
+    test_ostream->operator<<(input_num);
+    CHECK_EQUAL(out, test_device.test_input_str);
+}
+
+TEST(ostream, write_zero)
+{
+    test_ostream->operator<<(0);
+    CHECK_EQUAL("0", test_device.test_input_str);
+}
+
+TEST(ostream, write_unsigned_int)
+{
+    unsigned int input_num = 11111;
+    test_ostream->operator<<(input_num);
+    CHECK_EQUAL("11111", test_device.test_input_str);
+}
+
+TEST(ostream, write_unsigned_int_max)
+{
+    std::basic_stringstream<char> ss;
+    unsigned int input_num = UINT_MAX;
+    char out[255];
+
+    ss << input_num;
+    ss.get(out, sizeof(out));
+    test_ostream->operator<<(input_num);
+    CHECK_EQUAL(out, test_device.test_input_str);
+}
+
+TEST(ostream, write_unsigned_int_zero)
+{
+    unsigned int input_num = 0;
+    test_ostream->operator<<(input_num);
+    CHECK_EQUAL("0", test_device.test_input_str);
+}
+
+TEST(ostream, write_unsigned_int_error_case)
+{
+    test_device.error_mode = true;
+    unsigned int input_num = 11111;
+
+    mock().expectOneCall("write").andReturnValue(-1);
+    test_ostream->operator<<(input_num);
+    mock().checkExpectations();
+    CHECK_EQUAL("", test_device.test_input_str);
+}
+
+
+int main(int argc, char *argv[])
+{
+    return CommandLineTestRunner::RunAllTests(argc, argv);
+}
