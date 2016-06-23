3e66541d77a25eb992233cd584427be4b83eceb4
Max Payne <forgge@gmail.com>
2016-06-24 01:59:12 +0300

    Add STM32L1xx platform draft
    Most functionality in stubs or is not used. However, it builds.

---
+++ platform/stm32l1xx/export/platform/execution.h
@@ -0,0 +1,28 @@
+//! \file
+//! \brief Module aggregates routines that are control execution flow of the MCU.
+//!
+#ifndef THE_CORE_EXECUTION_H_
+#define THE_CORE_EXECUTION_H_
+
+
+//! \brief Waits in loop for a given amount of milliseconds.
+//! \details Uses DWT register.
+//! See http://www.carminenoviello.com/2015/09/04/precisely-measure-microseconds-stm32/
+//! \param[in] ms Milliseconds to wait
+static inline void ecl_spin_wait(uint32_t ms)
+{
+    // TODO: implement this routine
+    (void) ms;
+    for(;;);
+}
+
+// TODO #72: add add suspend() and resume() routines
+
+//! \brief Aborts execution of currently running code. Never return.
+static inline void ecl_abort()
+{
+    for(;;);
+}
+
+
+#endif // THE_CORE_EXECUTION_H_
