13b4305719a1d917dbb6c68643906f98b3055060
Max Payne <forgge@gmail.com>
2016-07-18 01:56:10 +0300

    stm32: MCU family info propagated to the rest of the system
    theCore would like to know which family is used.

--- platform/stm32/family/l1xx/export/stm32_device.hpp
+++ platform/stm32/family/l1xx/export/stm32_device.hpp
@@ -4,13 +4,13 @@
 
 #include "stm32l1xx.h"
 #include <core_cm3.h>
-
 #include "system_stm32l1xx.h"
 
 #include <stm32l1xx_usart.h>
 #include <stm32l1xx_rcc.h>
 #include <stm32l1xx_gpio.h>
 #include <stm32l1xx_i2c.h>
+#include <stm32l1xx_spi.h>
 
 #include "stm32l1xx_dma_wrap.hpp"
 
