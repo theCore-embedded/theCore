c185afac6ee1bb894759bdd476d9435096653334
Max Payne <forgge@gmail.com>
2016-09-04 14:28:35 +0300

    Missing dependenices in system modules

--- platform/stm32/linker/gnu/gnu.cmake
+++ platform/stm32/linker/gnu/gnu.cmake
@@ -1,4 +1,6 @@
-# Linker flags. TODO: check redundancy when using -msoft-float and -mfloat-abi=soft together
+# Linker flags.
+# TODO: check redundancy when using -msoft-float and -mfloat-abi=soft together
+# TODO: alternate flags according to choosen family.
 set(CMAKE_C_LINK_FLAGS
 	"-fuse-linker-plugin -mcpu=cortex-m3 -msoft-float -mfloat-abi=soft \
 	-nostartfiles -mthumb -flto -Wl,--gc-sections"
