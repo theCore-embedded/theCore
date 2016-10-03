ffefaee2ce508f4bbb72983164b7357bdd2afcff
Max Payne <forgge@gmail.com>
2016-10-04 00:11:09 +0300

    Fix double evaluation and indent in linker dispatcher

--- platform/common/linker.cmake
+++ platform/common/linker.cmake
@@ -1,14 +1,14 @@
 set(PLATFORM_DIR ${CORE_DIR}/platform/${PLATFORM_NAME}/)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 	# using Clang
 	include(${PLATFORM_DIR}linker/clang/clang.cmake)
-elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 	# using GCC
 	include(${PLATFORM_DIR}linker/gnu/gnu.cmake)
-elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
 	# using Intel C++
-elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "MSVC")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 	# using Visual Studio C++
 	# If you see this I encourage you to implement such support! :)
 	message(FATAL_ERROR "MSVC not yet supported!")
