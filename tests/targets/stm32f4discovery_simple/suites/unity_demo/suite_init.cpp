9b7d4efae72215b8703e89aba2a9a4bccd19b534
Max Payne <forgge@gmail.com>
2016-10-16 21:14:05 +0300

    Unity demo case / suite integrated with new test autogen system

--- tests/targets/stm32f4discovery_simple/suites/unity_demo/suite_init.cpp
+++ tests/targets/stm32f4discovery_simple/suites/unity_demo/suite_init.cpp
@@ -1,16 +1,8 @@
-//! \file
-//! \brief
-//! \todo Autogenerate this file
-
-#include <unity.h>
-#include <unity_fixture.h>
-
 #include <stm32f4xx_rcc.h>
 #include <stm32f4xx_usart.h>
 #include <stm32f4xx_gpio.h>
 
-extern "C"
-void board_init()
+void suite_board_init()
 {
     // Only console is required for this suite
 
@@ -26,22 +18,4 @@ void board_init()
 
     GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
     GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
-}
-
-TEST_GROUP_RUNNER(demo_group)
-{
-    RUN_TEST_CASE(demo_group, test1);
-}
-
-static void suite_runner()
-{
-    RUN_TEST_GROUP(demo_group);
-}
-
-int main()
-{
-    UNITY_BEGIN();
-    suite_runner();
-    UNITY_END();
-    for (;;);
 }
