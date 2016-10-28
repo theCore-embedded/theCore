824ad31c698cd91b49a5988e54fbcf6198256749
Max Payne <forgge@gmail.com>
2016-10-29 01:09:22 +0300

    Move common definitions to the platform UART driver
    Every platform should device its own, specific UART device definitions
    and define rules for that definitions. So that definitions are not
    'common' in any way

--- platform/stm32/export/aux/usart_bus.hpp
+++ platform/stm32/export/aux/usart_bus.hpp
@@ -23,6 +23,19 @@
 namespace ecl
 {
 
+//! Represents distinct peripheral devices
+enum class usart_device
+{
+    dev1,
+    dev2,
+    dev3,
+    dev4,
+    dev5,
+    dev6,
+    dev7,
+    dev8
+};
+
 //! Base template class for the usart configuration
 //! \details User must create template specialization for required USART device.
 //! Usart configuration class must contain following constexpr fields:
