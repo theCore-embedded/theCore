eadfd082ea1a5d4479bb5b06ce68e5bd457085d4
Max Payne <forgge@gmail.com>
2016-11-28 00:54:05 +0200

    Fix proper compiler, linker and standart deduction
    Standart can be set using toolchain file. For hosted compilation it is
    automatically set to C++14.

--- platform/stm32/linker/gnu/gnu.cmake
+++ platform/stm32/linker/gnu/gnu.cmake
@@ -1,8 +1,18 @@
 ﻿# Linker flags.
 # TODO: check redundancy when using -msoft-float and -mfloat-abi=soft together
-# TODO: alternate flags according to choosen family.
+
+if(CONFIG_PLATFORM_DEVICE MATCHES "STM32F4")
+    set(FAMILY_CORE "-mcpu=cortex-m4")
+elseif(CONFIG_PLATFORM_DEVICE MATCHES "STM32L1")
+    set(FAMILY_CORE "-mcpu=cortex-m3")
+else()
+    # Likely that additional implementation is required in order to add new stm32
+    # family
+    message(FATAL_ERROR "Device ${CONFIG_PLATFORM_DEVICE} doesn't have any linker flags defined")
+endif()
+
 set(CMAKE_C_LINK_FLAGS
-	"-fuse-linker-plugin -mcpu=cortex-m3 -msoft-float -mfloat-abi=soft \
-	-nostartfiles -mthumb -flto -Wl,--gc-sections"
+    "-fuse-linker-plugin ${FAMILY_CORE} -msoft-float -mfloat-abi=soft \
+    -nostartfiles -nostdlib -mthumb -flto -Wl,--gc-sections"
 	CACHE STRING "Linker C flags")
 set(CMAKE_CXX_LINK_FLAGS ${CMAKE_C_LINK_FLAGS} CACHE STRING "Linker C++ flags")
