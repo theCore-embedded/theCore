ce634ab64f04a7e66ba986942ac6407ba2e5f153
Max Payne <forgge@gmail.com>
2016-12-02 17:49:14 +0200

    tm4c: exti_manager BAT test

---
+++ tests/cases/exti_manager_bat/case.cpp
@@ -0,0 +1,82 @@
+#include <unity.h>
+#include <unity_fixture.h>
+
+#include <test_gpio.hpp>
+
+#include <platform/exti_manager.hpp>
+
+// Holds info about expectancy of handler call.
+static volatile bool                call_is_expected    = false;
+
+// True if test_exti_hanlder() call occur. Must be set to false before new call.
+static volatile bool                call_occur          = true;
+
+// Expected ctx during test_exti_hanlder() call.
+static void                         *expected_ctx       = nullptr;
+
+// Handler object.
+static ecl::exti_manager::handler   test_handler        = {};
+
+// Handles EXTI events during test
+static void test_exti_hanlder(void *ctx)
+{
+    TEST_ASSERT_TRUE(call_is_expected);
+    TEST_ASSERT_TRUE(ctx == expected_ctx);
+    TEST_ASSERT_FALSE(call_occur);
+
+    call_occur = true;
+}
+
+TEST_GROUP(exti_basic);
+
+TEST_SETUP(exti_basic)
+{
+    expected_ctx        = nullptr;
+    call_is_expected    = false;
+    call_occur          = false;
+
+    test_handler.set_cb(test_exti_hanlder);
+    test_handler.set_ctx(nullptr);
+
+    ecl::exti_manager::subscribe<ecl::push_button>(test_handler,
+        ecl::exti_manager::trigger::rising);
+}
+
+TEST_TEAR_DOWN(exti_basic)
+{
+    ecl::exti_manager::unsubscribe(test_handler);
+}
+
+TEST(exti_basic, subscribe_unsubscribe)
+{
+    // Both subscribe and unsubscribe take place in the setup and teardown
+}
+
+TEST(exti_basic, mask_unmask)
+{
+    ecl::exti_manager::mask(test_handler);
+    ecl::exti_manager::unmask(test_handler);
+}
+
+TEST(exti_basic, handle_presses)
+{
+    int test_ctx            = -1;
+    int calls_expected      = 3;
+    int calls_occur         = 0;
+
+    call_is_expected = true;
+    expected_ctx = &test_ctx;
+
+    test_handler.set_ctx(expected_ctx);
+
+    while (calls_occur < calls_expected) {
+        ecl::exti_manager::unmask(test_handler);
+        while (!call_occur) { } // Wait for a call
+
+        UnityPrint("Button press detected\n");
+
+        call_occur = false;
+
+        calls_occur++;
+    }
+}
--- tests/targets/tivac_tm4/suites/platform_bat/suite_init.cpp
+++ tests/targets/tivac_tm4/suites/platform_bat/suite_init.cpp
@@ -1,14 +1,22 @@
+//! \file
+//! \brief Suite initialization of the TI TIVAC (TM4C) platform BAT suite.
+
 #include <aux/pin_cfg.hpp>
 #include <pin_map.h>
 
 void suite_board_init()
 {
+    // GPIO LEDS
+
     ecl::gpio_hw::enable_periph<ecl::gpio_hw::port::f>();
 
     ecl::gpio_hw::apply_cfg<ecl::gpio_hw::port::f,
         ecl::gpio_hw::num::pin1,
         ecl::gpio_hw::num::pin2,
         ecl::gpio_hw::num::pin3>();
+
+    // GPIO push button
+    ecl::gpio_hw::apply_cfg<ecl::gpio_hw::port::f, ecl::gpio_hw::num::pin4>(ecl::gpio_hw::dir::in);
 
     // Console
     ecl::gpio_hw::enable_periph<ecl::gpio_hw::port::a>();
