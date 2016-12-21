5d9fff0b82bae3e61f6b53b63bda741c01033afe
Max Payne <forgge@gmail.com>
2016-12-21 22:44:03 +0200

    scripts: use  uncrustify as main source formatting tool

--- scripts/srcformat.sh
+++ scripts/srcformat.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-astyle  --style=1tbs \
-        --indent=spaces=4 \
-        --indent-switches \
-        --indent-preproc-define \
-        --indent-col1-comments \
-        --pad-oper \
-        --pad-header \
-        --align-pointer=name \
-        --align-reference=name \
-        --add-brackets \
-        --keep-one-line-blocks \
-        --convert-tabs \
-        --close-templates \
-        --max-code-length=80 \
-        $@
+script="$0"
+basename="$(dirname $script)"
+
+uncrustify -c "${basename}/srcformat.cfg" --replace $@
