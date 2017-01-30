e83aa5f5c69de44ab5c046f54302a7add44fa18c
Max Payne <forgge@gmail.com>
2017-01-30 02:02:30 +0200

    >#209 Specified system includes flag for cm3/cm4 toolchain
    Cmake won't set it by itself.

--- toolchains/arm-cm4-gnu.cmake
+++ toolchains/arm-cm4-gnu.cmake
@@ -1,4 +1,4 @@
-﻿# this one is important
+# this one is important
 set(CMAKE_SYSTEM_NAME Generic)
 # this one not so much
 set(CMAKE_SYSTEM_VERSION 1)
@@ -26,6 +26,10 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 ################################################################################
 # Flags and definitions used with GNU complilation suite
+
+# Essential stuff for using 3rd-party libs
+set(CMAKE_INCLUDE_SYSTEM_FLAG_C "-isystem ")
+set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
 
 # avoid using any additional flags when linking with shared libraries
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
