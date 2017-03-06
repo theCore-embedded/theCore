af587f84589bab24b1657e825950e0e729fdd29e
Max Payne <forgge@gmail.com>
2017-03-06 11:33:52 +0200

    Use libnano as std lib for M0+ MCU

--- platform/ke02/linker/gnu/gnu.cmake
+++ platform/ke02/linker/gnu/gnu.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_C_LINK_FLAGS
         "-fuse-linker-plugin -march=armv6s-m -mcpu=cortex-m0plus -nostartfiles \
-        -nodefaultlibs -nostdlib -mthumb -Wl,--gc-sections"
+        -specs=nano.specs -mthumb -Wl,--gc-sections"
         CACHE STRING "Linker C flags")
 set(CMAKE_CXX_LINK_FLAGS ${CMAKE_C_LINK_FLAGS} CACHE STRING "Linker C++ flags")
