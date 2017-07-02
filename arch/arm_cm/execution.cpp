72855363eb7ca6111469d2677080e9ff0266d5f6
Vadik Tkachuk <vadimol92@gmail.com>
2017-07-03 00:17:43 +0300

    Added compiler error check

--- arch/arm_cm/execution.cpp
+++ arch/arm_cm/execution.cpp
@@ -43,6 +43,8 @@ extern "C"  void __attribute__((naked)) arch_delay(uint32_t loop_count)
    __asm("    subs    r0, #1\n"
           "    bne    arch_delay\n"
           "    bx     lr");
+#else
+#error Not yet implemented for your compiler
 #endif
 }
 
