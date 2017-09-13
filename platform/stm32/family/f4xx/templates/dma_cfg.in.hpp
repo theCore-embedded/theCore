6e7a16ae98c8c784784fd7a5cd1dcdbf8518ca98
Max Payne <forgge@gmail.com>
2017-09-13 14:24:57 +0300

    >#239 Move DMA config template where it ought to be
    DMA depends heavily on family, so it should be placed into the family
    subdir

--- platform/stm32/family/f4xx/templates/dma_cfg.in.hpp
+++ platform/stm32/family/f4xx/templates/dma_cfg.in.hpp
@@ -1,18 +1,19 @@
 //! \file
 //! \brief Header provides template of DMA driver configuration
-#ifndef STM32F4XX_DMA_CFGS_HPP_
-#define STM32F4XX_DMA_CFGS_HPP_
+#ifndef STM32_DMA_CFGS_HPP_
+#define STM32_DMA_CFGS_HPP_
 
 #include <stm32_device.hpp>
 
-//namespace ecl
-//{
+namespace ecl
+{
 
 /*[[[cog
 import cog
 import json
 
 cfg = json.load(open(JSON_CFG))
+cfg = cfg['platform']
 
 dma = {}
 if 'dma' in cfg:
@@ -22,11 +23,16 @@ template_dma = 'using %s = ' \
     'ecl::dma_wrap<ecl::dma_stream::dma%d_%d, ecl::dma_channel::ch%d>;'
 
 for d in dma:
+    # Place comment line if needed
+    if 'comment' in d:
+        # Avoid using begin and end comment section tokens
+        cog.outl(('\n%s* ' + d['comment'] + ' *%s') % ('/', '/'))
+
     cog.outl(template_dma % (d['alias'], d['num'], d['stream'], d['channel']))
 
 ]]]*/
 //[[[end]]]
 
-//} // namespace ecl
+} // namespace ecl
 
-#endif // STM32F4XX_DMA_CFGS_HPP_
+#endif // STM32_DMA_CFGS_HPP_
