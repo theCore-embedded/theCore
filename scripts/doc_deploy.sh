f66259170ff99d1441d6eda72f6eedadda2b3d54
Max Payne <forgge@gmail.com>
2017-11-26 18:40:54 +0200

    >277 Move doc deploy to after_success travis section

--- scripts/doc_deploy.sh
+++ scripts/doc_deploy.sh
@@ -4,6 +4,17 @@
 set -e # Stop on error
 set -v
 
+echo "Travis PR flag: $TRAVIS_PULL_REQUEST"
+echo "Travis branch: $TRAVIS_BRANCH"
+echo "Travis commit: $TRAVIS_COMMIT"
+echo "Travis build number: $TRAVIS_BUILD_NUMBER"
+
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" -o "${TRAVIS_BRANCH}" != "develop" ]; then
+    echo "Skipping doc deploy. Only develop branch should be deployed"
+    exit 0
+fi
+
 BRANCH_NAME="travis-${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}"
 
 cd build
@@ -20,7 +31,7 @@ cp -rv ../docs/sphinx/* .
 
 if [ -z "$(git status --porcelain)" ]; then
     echo "No documentation changes present. Skipping doc deploy"
-    return
+    exit 0
 fi
 
 git checkout -b ${BRANCH_NAME}
