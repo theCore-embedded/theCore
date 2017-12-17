60e7b7811366785d97d7e175e780314a204255fb
Max Payne <forgge@gmail.com>
2017-12-17 10:11:43 +0200

    Add missing sources for version4git

---
+++ lib/utils/version.in.cpp
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+namespace ecl
+{
+#if @THECORE_VER4GIT_FOUND@
+
+#if @PROJECT_GIT_DIRTY@
+#define DIRTY "-dirty"
+#else
+#define DIRTY ""
+#endif
+
+const char *ver_str = "@PROJECT_NAME@ v@PROJECT_VERSION@ " "@PROJECT_GIT_SHORT@" DIRTY;
+const char *ver_commit = "@PROJECT_GIT_COMMIT@";
+const char *ver_branch = "@PROJECT_GIT_BRANCH@";
+const char *ver_url = "@PROJECT_GIT_URL@";
+
+#else // ! THECORE_VER4GIT_FOUND
+
+const char *ver_str     = "v0.3.0";
+const char *ver_commit  = "";
+const char *ver_branch  = "";
+const char *ver_url     = "";
+
+#endif // THECORE_VER4GIT_FOUND
+} // namespace ecl
