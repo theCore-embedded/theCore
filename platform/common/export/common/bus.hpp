675af0e719740459b56573fad07295ac7942fe0e
Max Payne <forgge@gmail.com>
2018-02-19 15:39:25 +0200

    add platform skel

--- platform/common/export/common/bus.hpp
+++ platform/common/export/common/bus.hpp
@@ -3,9 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 //! \file
-//! \brief Platform bus example interface.
-#ifndef PLATFORM_COMMON_EXAMPLE_BUS_
-#define PLATFORM_COMMON_EXAMPLE_BUS_
+//! \brief Platform bus common interface.
+#ifndef PLATFORM_COMMON_BUS_
+#define PLATFORM_COMMON_BUS_
 
 #include <ecl/err.hpp>
 
@@ -48,64 +48,7 @@ enum class async_type
 //!
 using bus_handler = std::function<void(bus_channel ch, bus_event type, size_t total)>;
 
-//! The dummy bus class.
-//! \details Provided just as an API example of platform-level bus.
-class dummy_platform_bus
-{
-public:
-    //! Lazy initialization.
-    //! \return Status of operation.
-    static err init()
-    { return err::nosys; }
-
-    //! Sets rx buffer with given size.
-    //! \param[in,out]  rx      Buffer to write data to. Optional.
-    //! \param[in]      size    Size
-    static void set_rx(uint8_t *rx, size_t size)
-    { (void) rx; (void) size; }
-
-    //! Sets rx buffer made-up from sequence of similar bytes.
-    //! \param[in] size         Size of sequence
-    //! \param[in] fill_byte    Byte to fill a sequence. Optional.
-    static void set_tx(size_t size, uint8_t fill_byte = 0xff)
-    { (void) size; (void) fill_byte; }
-
-    //! Sets tx buffer with given size.
-    //! \param[in] tx   Buffer to transmit. Optional.
-    //! \param[in] size Buffer size.
-    static void set_tx(const uint8_t *tx, size_t size)
-    { (void) tx; (void) size; }
-
-    //! Sets event handler.
-    //! Handler will be used by the bus, until reset_handler() will be called.
-    //! \param[in] handler Handler itself.
-    static void set_handler(const bus_handler &handler)
-    { (void) handler; }
-
-    //! Reset xfer buffers.
-    //! \details Buffers that were set by \sa set_tx() and \sa set_rx()
-    //! will be no longer used after this call.
-    static void reset_buffers()
-    { return; }
-
-    //! Resets previously set handler.
-    static void reset_handler()
-    { return; }
-
-    //! Executes xfer, using buffers previously set.
-    //! \details When it will be done, handler will be invoked.
-    //! \return Status of operation.
-    static err do_xfer()
-    { return err::nosys; }
-
-    //! Cancels xfer.
-    //! \details After this call no xfer will occur.
-    //! \return Status of operation.
-    static err cancel_xfer()
-    { return err::nosys; }
-};
-
 } // namespace ecl
 
 
-#endif // PLATFORM_COMMON_EXAMPLE_BUS_
+#endif // PLATFORM_COMMON_BUS_
---
+++ platform/skel/templates/spi_cfg.in.hpp
@@ -0,0 +1,112 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! \file
+//! \brief Header provides template of SPI and spi drivers configuration.
+#ifndef PLATFORM_SPI_CFGS_HPP_
+#define PLATFORM_SPI_CFGS_HPP_
+
+#include <aux/platform_defines.hpp>
+#include <aux/spi.hpp>
+
+namespace ecl
+{
+
+//! \addtogroup platform Platform defintions and drivers
+//! @{
+
+//! \addtogroup skel Platform skeleton
+//! @{
+
+//! \defgroup skel_templates Auto-generated code, Python COG and CMake templates
+//! for platform skeleton.
+//! @{
+
+/*[[[cog
+import cog
+import json
+
+cfg = json.load(open(JSON_CFG))
+cfg = cfg['platform']
+
+]]]*/
+//[[[end]]]
+
+// SPI configuration -----------------------------------------------------------
+
+/*[[[cog
+
+# Maps SPI config values to correct definitions
+spi_mapping = {
+    'type' : {
+        'master'        : 'ecl::spi_type::master',
+        # TODO: slave mode is not implemented
+        # 'slave'       : 'ecl::spi_type::slave'
+    },
+    'cpol': {
+        'high'          : 'ecl::spi_cpol::high',
+        'low'           : 'ecl::spi_cpol::low'
+    },
+    'cpha': {
+        'high'          : 'ecl::spi_cpha::high',
+        'low'           : 'ecl::spi_cpha::low'
+    },
+}
+
+# Template for SPI config
+spi_cfg_struct = '''
+template<>
+struct spi_cfg<spi_channel::ch%(spi_ch)d>
+{
+    static constexpr auto type = %(spi_type)s;
+    static constexpr auto cpol = %(spi_cpol)s;
+    static constexpr auto cpha = %(spi_cpha)s;
+};
+
+'''
+
+# Template for driver typedef
+spi_typedef = 'using %s = spi<spi_device::bus%d>;'
+# Alias template (optional)
+spi_alias = 'using %s = %s;'
+
+spi_cfgs = {}
+if 'spi' in cfg:
+    spi_cfgs = cfg['spi']
+
+for spi in spi_cfgs:
+    id = spi['id']
+    spi_name = 'spi_' + str(id) + '_driver'
+
+    # Directly included values
+    values = {
+        'spi_ch' : id,
+    }
+
+    # Values that require mapping
+    for k, v in spi_mapping.items():
+        json_value = spi[k]
+        values[k] = v[json_value]
+
+    if 'comment' in spi:
+        cog.outl('/' + '* ' + spi['comment'] + ' *' + '/')
+
+    cog.outl(spi_cfg_struct % values)
+    cog.outl(spi_typedef % (spi_name, id))
+
+    if 'alias' in spi:
+        cog.outl(spi_alias % (spi['alias'], spi_name))
+
+]]]*/
+//[[[end]]]
+
+//! @}
+
+//! @}
+
+//! @}
+
+} // namespace ecl
+
+#endif // STM32XX_SPI_spi_CFGS_HPP_
