e2b6ca68f31c209f9ba18b6552da66676562d5ef
Max Payne <forgge@gmail.com>
2018-03-01 19:19:54 +0200

    Separate documentation dependencies into separate category in Nix
    Build time greatly reduces now, if only development dependenices are
    required.

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 # TODO: comments
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import <nixpkgs> {},
+  docs ? false # Build dependencies required to generate documentation
 }:
 
 let
@@ -7,29 +8,28 @@ let
   fulltoc = pkgs.callPackage ./nix/sphinx-fulltoc {};
   cmake-doxygen = pkgs.callPackage ./nix/cmake-doxygen {};
   cmake-version4git = pkgs.callPackage ./nix/cmake-version4git {};
-
+  # Additionall documentation dependencies take enourmous time to download
+  # and build. They are not needed by default.
+  docs_deps = with pkgs; with python35Packages; if docs then
+                [ sphinx
+                  sphinx_rtd_theme
+                  recommonmark
+                  plantuml
+                  graphviz
+                  fulltoc
+                  doxygen
+                  cmake-doxygen ]
+              else
+                  [ ];
 in with pkgs; with python35Packages; {
   coreEnv = stdenv.mkDerivation {
     name = "thecore";
     hardeningDisable = [ "all" ];
     buildInputs = [
-      # With Python configuration requires a special wrapper
-#      (python35.buildEnv.override {
-#        ignoreCollisions = true;
-#        extraLibs = with python35Packages; [
-          cogapp
-          jsonschema
-          sphinx
-          sphinx_rtd_theme
-          recommonmark
-#        ];
-#      })
-
-      which cmake gcc6 gdb cppcheck git
+      cogapp jsonschema which cmake gcc6 gdb cppcheck git
       cpputest gcc-arm-embedded-5 dfu-util
-      doxygen  llvmPackages.clang-unwrapped openocd
-      perlPackages.ArchiveZip xxd
-      fulltoc cmake-doxygen cmake-version4git plantuml graphviz ];
+      llvmPackages.clang-unwrapped openocd
+      perlPackages.ArchiveZip xxd cmake-version4git ] ++ docs_deps;
 
     shellHook = ''
         export GCC_ARM_EMBEDDED_PATH=${gcc-arm-embedded-5}
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ export SSL_CERT_FILE=/root/.nix-profile/etc/ssl/certs/ca-bundle.crt
 echo $SSL_CERT_FILE
 
 if [ -n "$*" ]; then
-    nix-shell /expr --run "$*"
+    nix-shell --arg docs true /expr --run "$*"
 else
-    nix-shell /expr
+    nix-shell --arg docs true /expr
 fi
