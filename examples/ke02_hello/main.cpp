f627f2bc2390619635963388884c7e3ee6ea974a
Max Payne <forgge@gmail.com>
2018-04-07 23:17:47 +0300

    ke02: add stub example to perform kind-of-a-testing

---
+++ examples/ke02_hello/main.cpp
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <ecl/iostream.hpp>
+#include <ecl/assert.h>
+
+extern "C"
+void board_init()
+{
+
+}
+
+int main()
+{
+    ecl::cout << "Hello, Host World!" << ecl::endl;
+    ecl_assert_msg(1 == 0, "This is an assert demonstration. Application will abort.");
+    return 0;
+}
